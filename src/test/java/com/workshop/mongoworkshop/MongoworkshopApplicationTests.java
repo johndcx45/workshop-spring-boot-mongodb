e4aa527bcb0757b3687472393fcc174e2477b0d5
Joao Pedro <johndcx45@gmail.com>
2020-03-13 13:56:31 -0300

    Project created

---
+++ src/test/java/com/workshop/mongoworkshop/MongoworkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.mongoworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoworkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
